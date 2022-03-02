7902d4e60e6c4eccc61eb2a37961f34943bfd311
이재만 <lbefull@naver.com>
2022-03-02 15:01:58 +0900

    #S06P21S001-39 feat: install module & login page
    Former-commit-id: 5db8a05401eba81b9dec0c2f0b097f1561830673

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
