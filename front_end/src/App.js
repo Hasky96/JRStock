6a37f59bccf009b0aa49c64ab4009fe197c80370
이재만 <lbefull@naver.com>
2022-03-03 15:59:26 +0900

    #S06P21S001-46 feat: login page & signup page
    Former-commit-id: 1ec8bd0e7cf7fc11cb841b03917896918150d481

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,26 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
+import Signup from "./routes/Signup";
 import SideBar from "./components/SideBar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div>
+      {/* <div>
         <SideBar></SideBar>
       </div>
       <div>
         <div>
           <Header></Header>
-        </div>
+        </div> */}
         <Router>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </Router>
-      </div>
+      {/* </div> */}
     </>
   );
 }
