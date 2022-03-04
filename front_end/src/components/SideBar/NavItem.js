b60f213d568019f1055a96f972ea7c6320589421
j1h00 <guruboy@snu.ac.kr>
2022-03-04 14:53:07 +0900

    #S06P21S001-39 feat: Edit SideBar (active when hover)
    Former-commit-id: 18d639c83a48473b3b4b1f4db698c949ef99f4a0 [formerly cf7f309b8c454cedc69f41868f027aaf3712d0f7] [formerly 97985ef83313a19807c8f223a04287b63fb20a3a [formerly 6d7a625a2f879ec4b772a83ba1f139d20e2684c1]]
    Former-commit-id: 97985ef83313a19807c8f223a04287b63fb20a3a
    Former-commit-id: a0462cf51436e60bbbd6cd3ffd2513297f37a5ad

---
+++ front_end/src/components/SideBar/NavItem.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./SideBar.css";
+
+export default function NavItem({ currentPath, linkPath, linkText, children }) {
+  const active = "nav-link";
+  const inActive = "nav-link grayscale hover:grayscale-0";
+
+  return (
+    <li className="nav-item hover:bg-yellow-50">
+      <Link to={linkPath}>
+        <div className={currentPath === linkPath ? active : inActive}>
+          {children}
+          <span className="link-text">{linkText}</span>
+        </div>
+      </Link>
+    </li>
+  );
+}
