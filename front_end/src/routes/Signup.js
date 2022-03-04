80cc643fa055572858219dc3fee49a008094416e
Ji Wook Jeong <doksakim77@naver.com>
2022-03-04 15:11:48 +0900

    #S06P21S001-42 feat: add regex.js and Edit Signup.js
    Former-commit-id: ba8dd085bb0d506881a841b93fd2b37b854def93 [formerly a8ee73aa013729662144fafa4efe68c55cfd9dfe] [formerly 6a41be1f848e2ca52fb7de45fe893043fc472e53 [formerly dae8535433d4e3bb50a1992751aeb6a8b16eb128]]
    Former-commit-id: 6a41be1f848e2ca52fb7de45fe893043fc472e53
    Former-commit-id: 3804bc4c078cd0ba9b503c3a3c3b9ce1b256c9af

--- front_end/src/routes/Signup.js
+++ front_end/src/routes/Signup.js
@@ -1,30 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./background.css";
+import { validEmail } from "./../regex";
 
-export default function Login() {
+export default function Signup() {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [password2, setPassword2] = useState(null);
+  const [passwordErr, setPasswordErr] = useState(false);
+  const [emailErr, setEmailErr] = useState(false);
+  const [duplicateCheck, setDuplicateCheck] = useState(false);
+
   return (
     <div>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-img">
         <div className="max-w-lg w-full space-y-8 border-2 p-12 rounded-md bg-white">
+          {/* title */}
           <div>
-            <h2 className="text-3xl font-extrabold text-gray-900 text-center">JRstock</h2>
-          </div>
-          <div>
-            <h2 className="text-xl text-gray-900">
-              Create an account
+            <h2 className="text-3xl font-extrabold text-yellow-900 text-center">
+              JRstock
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600"></p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          {/* 사용자 정보 입력 폼 */}
+          <form
+            className="mt-8 space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (emailErr) alert("유효한 이메일을 입력하세요!");
+              else if (!duplicateCheck) alert("이메일 중복을 체크해주세요");
+              else if (passwordErr) alert("비밀번호를 올바르게 입력하세요!");
+              else {
+                // 회원가입 정보 보내기
+                alert("회원가입 정보 전송!");
+              }
+            }}
+          >
+            {/* hidden input .. 임시로 남겨둠 */}
             <input type="hidden" name="remember" defaultValue="true" />
+            {/* 이름 input 부분 */}
             <div className="rounded-md shadow-sm">
               <div className="my-5">
                 <label htmlFor="name">이름</label>
@@ -33,11 +49,16 @@ export default function Login() {
                   name="name"
                   type="text"
                   autoComplete="on"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border border-yellow-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
                   placeholder="Name"
                 />
               </div>
+              {/* 이메일 input 부분 */}
               <div className="my-5">
                 <label htmlFor="email-address">이메일</label>
                 <input
@@ -45,12 +66,42 @@ export default function Login() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  value={email}
+                  onChange={(e) => {
+                    // 이메일 유효성 확인
+                    if (validEmail.test(e.target.value)) {
+                      setEmailErr(false);
+                    } else {
+                      setEmailErr(true);
+                    }
+                    setEmail(e.target.value);
+                    setDuplicateCheck(false);
+                  }}
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border border-yellow-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
                 />
-                <button className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-emerald-400 hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">이메일 인증</button>
+                {emailErr && (
+                  <p className="text-red-500">
+                    이메일 정보가 유효하지 않습니다.
+                  </p>
+                )}
+                {/* 이메일 중복 확인 버튼 */}
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    // 이메일 중복 확인 요청 보내기
+                    setDuplicateCheck(true);
+                  }}
+                  className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-300 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                >
+                  이메일 중복 확인
+                </button>
+                {duplicateCheck && (
+                  <p className="text-yellow-500">이용 가능한 이메일 입니다.</p>
+                )}
               </div>
+              {/* 비밀번호 input 부분 */}
               <div className="my-5">
                 <label htmlFor="password">비밀번호</label>
                 <input
@@ -58,11 +109,19 @@ export default function Login() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => {
+                    // 비밀번호 일치 여부 검증
+                    if (e.target.value === password2) setPasswordErr(false);
+                    else setPasswordErr(true);
+                    setPassword(e.target.value);
+                  }}
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border border-yellow-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
                 />
               </div>
+              {/* 비밀번호 확인 input 부분 */}
               <div className="my-5">
                 <label htmlFor="password-confirmation">비밀번호 확인</label>
                 <input
@@ -70,22 +129,33 @@ export default function Login() {
                   name="password-confirmation"
                   type="password"
                   autoComplete="current-password"
+                  onChange={(e) => {
+                    // 비밀번호 일치 여부 검증
+                    if (e.target.value === password) setPasswordErr(false);
+                    else setPasswordErr(true);
+                    setPassword2(e.target.value);
+                  }}
                   required
-                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border border-yellow-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
                 />
+                {passwordErr && (
+                  <p className="text-red-500">비밀번호가 일치하지 않습니다.</p>
+                )}
               </div>
             </div>
+            {/* 회원가입 버튼 */}
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-emerald-400 hover:bg-emerald-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-300 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
                 회원가입
               </button>
             </div>
           </form>
+          {/* 로그인 이동 부분 */}
           <div className="flex justify-center">
             <p>이미 계정이 있으신가요?&nbsp;</p>
             <Link to="/login" className="text-blue-600 hover:text-blue-500">
