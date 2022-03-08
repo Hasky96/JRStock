d63def60f5415b134278d362def2682f5be958d7
안영원 <kublaihkan@gmail.com>
2022-03-08 11:58:23 +0900

    #S06P21S001-179 feat: 유저 비밀번호 확인 API 구현
    Swagger에 JWT Token 넣을 수 있게 설정
    
    
    Former-commit-id: 23abce6738679102359529c6eb13ecf210e4664d [formerly f93b307510eae08254e3fa58ba89746290b10429] [formerly 3864d9f8e20223a3e7d86a60fcf578416d332028 [formerly dceda12a9f2ef035777eaeab2fb725415a05ce81]]
    Former-commit-id: 3864d9f8e20223a3e7d86a60fcf578416d332028
    Former-commit-id: 1b987d57a0cf41688d7f1dde0bb25d1acc2252c2

--- backend/JRstock/settings.py
+++ backend/JRstock/settings.py
@@ -87,6 +87,16 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS' :{
+        'Bearer':{
+            'type':'apiKey',
+            'name':'Authorization',
+            'in':'header'
+        }
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
