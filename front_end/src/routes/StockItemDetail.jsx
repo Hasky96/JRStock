133bb4feb752088f04d757288ea88997d1548f21
이재만 <lbefull@naver.com>
2022-03-14 13:23:41 +0900

    #S06P21S001-238 feat: 종목 상세 페이지 탭 분리
    Former-commit-id: 5c458c2b80f549df09b7abc35c54a9388c06fd76 [formerly 82a28f3d59698eefd7a9664f3799045f914998ac] [formerly 6c6a2ef2a60e0d03d567de43ebc0801669383084 [formerly 703e9704f513a82d39ce4a99aad4854fc41656e9]]
    Former-commit-id: 6c6a2ef2a60e0d03d567de43ebc0801669383084
    Former-commit-id: f9a9eb43425a7cfe13c5e7e5f943ff455c749b50

--- front_end/src/routes/StockItemDetail.jsx
+++ front_end/src/routes/StockItemDetail.jsx
@@ -2,13 +2,18 @@ import PageContainer from "../components/PageContainer";
 import { useParams } from "react-router-dom";
 import TabBar from "../components/TabBar/TabBar";
 import { useState } from "react";
+import Chart from "../components/StockItemDetail/Chart";
+import News from "../components/StockItemDetail/News";
+import Board from "../components/StockItemDetail/Board";
 
 export default function StockItemDetail() {
-  const { id } = useParams();
-  console.log(id);
+  // const { id } = useParams();
+  // console.log(id);
 
-  const [currentTab, setCurrentTab] = useState("1일");
-  const tabInfo = ["1일", "1주", "1개월", "3개월", "1년", "3년", "10년"];
+  const [currentTab, setCurrentTab] = useState("종합정보");
+  const tabInfo = ["종합정보", "뉴스", "종목토론 게시판"];
+
+  // console.log(currentTab);
 
   const stock = {
     id: 1351,
@@ -26,64 +31,74 @@ export default function StockItemDetail() {
 
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-9">
-        <PageContainer pr={2.5} minH={60}>
+      <div className={currentTab === "종합정보" ? "col-span-9" : "col-span-12"}>
+        <PageContainer
+          pr={currentTab === "종합정보" ? 2.5 : undefined}
+          minH={currentTab === "종합정보" ? 60 : undefined}
+        >
           <TabBar setCurrentTab={setCurrentTab} tabInfo={tabInfo} />
+          {currentTab === "종합정보" && <Chart />}
+          {currentTab === "뉴스" && <News />}
+          {currentTab === "종목토론 게시판" && <Board />}
         </PageContainer>
       </div>
-      <div className="col-span-3">
-        <PageContainer pl={2.5} pr={10} minH={60}></PageContainer>
-      </div>
-      <div className="col-span-12">
-        <PageContainer pt={5} minH={20}>
-          <div className="grid grid-cols-11">
-            <div className="col-span-3 flex justify-between">
-              <div>현재가</div>
-              <div>124</div>
+      {currentTab === "종합정보" && (
+        <div className="col-span-3">
+          <PageContainer pl={2.5} pr={10} minH={60}></PageContainer>
+        </div>
+      )}
+      {currentTab === "종합정보" && (
+        <div className="col-span-12">
+          <PageContainer pt={5} minH={20}>
+            <div className="grid grid-cols-11">
+              <div className="col-span-3 flex justify-between">
+                <div>현재가</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-5 col-end-8 flex justify-between">
+                <div>전일대비</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-9 col-end-12 flex justify-between">
+                <div>등락률</div>
+                <div>124</div>
+              </div>
+              <div className="col-span-3 flex justify-between">
+                <div>전일</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-5 col-end-8 flex justify-between">
+                <div>고가</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-9 col-end-12 flex justify-between">
+                <div>거래량</div>
+                <div>124</div>
+              </div>
+              <div className="col-span-3 flex justify-between">
+                <div>시가</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-5 col-end-8 flex justify-between">
+                <div>저가</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-9 col-end-12 flex justify-between">
+                <div>거래대금</div>
+                <div>124</div>
+              </div>
+              <div className="col-span-3 flex justify-between">
+                <div>52주 최고</div>
+                <div>124</div>
+              </div>
+              <div className="col-start-5 col-end-8 flex justify-between">
+                <div>52주 최저</div>
+                <div>124</div>
+              </div>
             </div>
-            <div className="col-start-5 col-end-8 flex justify-between">
-              <div>전일대비</div>
-              <div>124</div>
-            </div>
-            <div className="col-start-9 col-end-12 flex justify-between">
-              <div>등락률</div>
-              <div>124</div>
-            </div>
-            <div className="col-span-3 flex justify-between">
-              <div>전일</div>
-              <div>124</div>
-            </div>
-            <div className="col-start-5 col-end-8 flex justify-between">
-              <div>고가</div>
-              <div>124</div>
-            </div>
-            <div className="col-start-9 col-end-12 flex justify-between">
-              <div>거래량</div>
-              <div>124</div>
-            </div>
-            <div className="col-span-3 flex justify-between">
-              <div>시가</div>
-              <div>124</div>
-            </div>
-            <div className="col-start-5 col-end-8 flex justify-between">
-              <div>저가</div>
-              <div>124</div>
-            </div>
-            <div className="col-start-9 col-end-12 flex justify-between">
-              <div>거래대금</div>
-              <div>124</div>
-            </div>
-            <div className="col-span-3 flex justify-between">
-              <div>52주 최고</div>
-              <div>124</div>
-            </div>
-            <div className="col-start-5 col-end-8 flex justify-between">
-              <div>52주 최저</div>
-              <div>124</div>
-            </div>
-          </div>
-        </PageContainer>
-      </div>
+          </PageContainer>
+        </div>
+      )}
     </div>
   );
 }
