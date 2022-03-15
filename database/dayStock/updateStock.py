f0e3d5a8c3defa6f52e205ff3278e46b61f5539c
kjin <kjin4_1@naver.com>
2022-03-15 22:16:04 +0900

    #S06P21S001-185 feat: DB 주식 전종목 일봉 데이터 업데이트
    Former-commit-id: cf8008bbea574dd7d769fc4cfc651ac92dc64a81 [formerly ad97db4f3524fe9fb62d7848695da3865a003c12] [formerly 175027c846e3cbf119c82d24dd68a18c9dc55f15 [formerly ac8f10969b1b937af4fbfc750c7ddc0ebba1f534]]
    Former-commit-id: 175027c846e3cbf119c82d24dd68a18c9dc55f15
    Former-commit-id: 003f1ee53f9841a7d37c1cac4c9c36688b6a9ecf

--- database/dayStock/updateStock.py
+++ database/dayStock/updateStock.py
@@ -1,46 +1,44 @@
-# 실시간 데이터 받아오기
+# pip install openpyxl
 
-# import
+
 import requests
 import pandas as pd
-from io import BytesIO
-path = 'C:/ssafy/project2/data/day_stock'
+import io
 
-def krx_basic(tdate):
-    gen_req_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
-    query_str_parms = {
-        'mktId': 'ALL',
-        'trdDd': str(tdate),
-        'share': '1',
-        'money': '1',
-        'csvxls_isNo': 'false',
-        'name': 'fileDown',
-        'url': 'dbms/MDC/STAT/standard/MDCSTAT01501'
-    }
-    headers = {
-        'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'O2Mc1EmFLez8by7BX0EIh30Rkd32XD+7w/aGiWTvNHQ=' #generate.cmd에서 찾아서 입력하세요
-    }
-    r = requests.get(gen_req_url, query_str_parms, headers=headers)
-    gen_req_url = 'http://data.krx.co.kr/comm/fileDn/download_excel/download.cmd'
-    form_data = {
-        'code': r.content
-    }
-    r = requests.post(gen_req_url, form_data, headers=headers)
-    df = pd.read_excel(BytesIO(r.content))
-    df['일자'] = tdate
-    file_name = 'basic_'+ str(tdate) + '.xlsx'
-    df.to_excel(path+file_name, index=False, index_label=None)
-    print('KRX crawling completed :', tdate)
-    return
+tdate=20220314
+gen_req_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+query_str_parms = {
+    'mktId': 'ALL',
+    'trdDd': str(tdate),
+    'share': '1',
+    'money': '1',
+    'csvxls_isNo': 'false',
+    'name': 'fileDown',
+    'url': 'dbms/MDC/STAT/standard/MDCSTAT01501'
+}
+headers = {
+    'Referer': 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'O2Mc1EmFLez8by7BX0EIh30Rkd32XD+7w/aGiWTvNHQ=' #generate.cmd에서 찾아서 입력하세요
+}
+r = requests.get(gen_req_url, query_str_parms, headers=headers)
+gen_req_url = 'http://data.krx.co.kr/comm/fileDn/download_excel/download.cmd'
+form_data = {
+    'code': r.content
+}
+r = requests.post(gen_req_url, form_data, headers=headers)
+# print(BytesIO(r.content))
+bio = io.BytesIO(r.content) ## Some random BytesIO Object
+df = pd.read_excel(bio)
+# df2=pd.DataFrame(df)
 
+# print(df2)
+insert_str='insert into day_stock (`code_number`,`current_price`,`changes`,`chages_ratio`,`start_price`,`high_price`,`low_price`,`volume`,`trade_price`,`market_cap`,`stock_amount`,`date`) VALUES '
 
-krx_basic(20220314)
+for idx, row in df.iterrows():
+    # print(row['거래대금'])
+    insert_str+='("'+row['종목코드']+'",'+str(int(row['종가']))+','+str(row['대비'])+','+str(row['등락률'])+','+str(int(row['시가']))+','+str(int(row['고가']))+','+str(int(row['저가']))+','+str(int(row['거래량']))+','+str(int(row['거래대금']))+','+str(int(row['시가총액']))+','+str(int(row['상장주식수']))+','+str(tdate)+'),\n'
+    # break
+insert_str=insert_str[0:-2]+';'
+print(insert_str)
 
-# for year in range(2020, 2021):
-#     for month in range(1, 13):
-#         for day in range(1, 32):
-#             tdate = year * 10000 + month * 100 + day * 1
-#             if tdate <= 20211231:
-#                 krx_basic(tdate)
