87eb7e7bb2e4b5eec64ba634d69d6e981fb8e754
이재만 <lbefull@naver.com>
2022-03-15 15:32:36 +0900

    #S06P21S001-234 feat: 토론 & 뉴스 페이지 초안, 상세페이지 차트 추가
    Former-commit-id: 0a280811548a6ed5dfd559bcaa862a833abecdb1 [formerly 867cb585a96c6eeeaea908ba46440fc0483acab6] [formerly b850aecccdb78398c428767b17bc2220a98a1897 [formerly b2c48ea95afc26a8442a39ba4d52ddfcdeb76f5f]]
    Former-commit-id: b850aecccdb78398c428767b17bc2220a98a1897
    Former-commit-id: 9a70623a9e9a8e724a32903f06f64babe1ef01e3

--- front_end/src/components/StockItemDetail/Chart.jsx
+++ front_end/src/components/StockItemDetail/Chart.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+import LineChart from "../LineChart";
+import {
+  dayData,
+  weekData,
+  monthData,
+  yearData,
+} from "../../assets/marketChartTestData";
+import "../PageContainer.css";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export default function Chart() {
-  return <div>Chart</div>;
+  const [data, setData] = useState(dayData);
+  const [period, setPeriod] = useState("1D");
+  let seriesesData = new Map([
+    ["1D", dayData],
+    ["1W", weekData],
+    ["1M", monthData],
+    ["1Y", yearData],
+  ]);
+
+  const btnList = () => {
+    const list = [];
+    const intervals = ["1D", "1W", "1M", "1Y"];
+    intervals.forEach((el, idx) => {
+      list.push(
+        <button
+          key={idx}
+          onClick={(e) => {
+            e.preventDefault();
+            setData(seriesesData.get(e.target.innerText));
+            setPeriod(e.target.innerText);
+          }}
+          className={classNames(
+            "switcher-item",
+            period === el ? "switcher-active-item" : ""
+          )}
+        >
+          {el}
+        </button>
+      );
+    });
+    return list;
+  };
+
+  return (
+    <div className="pt-1 grid grid-cols-1">
+      <div className="grid border-2 my-h-50 grid-rows-6 rounded-xl m-2 p-3">
+        <div className="grid row-span-5">
+          <LineChart data={data} />
+        </div>
+        <div className="switcher row-span-1 pt-8">{btnList()}</div>
+      </div>
+    </div>
+  );
 }
--- front_end/src/routes/StockItemDetail.jsx
+++ front_end/src/routes/StockItemDetail.jsx
@@ -22,12 +22,29 @@ export default function StockItemDetail() {
     currentPrice: "72400",
     volatility: 200,
     volatilityRate: "+0.28%",
+    yesterdayPrice: "72200",
     volume: "17640567",
     marketPrice: "72400",
     highPrice: "72400",
     lowPrice: "72400",
     marketCapitalization: "430421300000000",
+    transactionPrice: "70000",
+    highPrice52: "90000",
+    lowPrice52: "60000",
   };
+
+  const statement = {
+    PER: "41.14배",
+    EPS: "2540원",
+    estimationPER: "53.80배",
+    estimationEPS: "1933원",
+    PBR: "5.15배",
+    BPS: "20276원",
+    dividendRate: "0.05%",
+    foreignLimit: "446112896",
+    foreignOwn: "126976103",
+    foreignRunout: "28.46%"
+  }
 
   return (
     <div className="grid grid-cols-12">
@@ -44,7 +61,50 @@ export default function StockItemDetail() {
       </div>
       {currentTab === "종합정보" && (
         <div className="col-span-3">
-          <PageContainer pl={2.5} pr={10} minH={60}></PageContainer>
+          <PageContainer pl={2.5} pr={10} minH={60}>
+            <div className="font-bold mb-2">{stock.name}</div>
+            <div className="flex justify-between">
+              <div>PER</div>
+              <div>{statement.PER}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>EPS</div>
+              <div>{statement.EPS}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>추정 PER</div>
+              <div>{statement.estimationPER}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>추정 EPS</div>
+              <div>{statement.estimationEPS}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>PBR</div>
+              <div>{statement.PBR}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>BPS</div>
+              <div>{statement.BPS}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>배당수익률</div>
+              <div>{statement.dividendRate}</div>
+            </div>
+            <hr className="my-2"/>
+            <div className="flex justify-between">
+              <div>외국인한도주식수</div>
+              <div>{statement.foreignLimit}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>외국인보유주식수</div>
+              <div>{statement.foreignOwn}</div>
+            </div>
+            <div className="flex justify-between">
+              <div>외국인소진율</div>
+              <div>{statement.foreignRunout}</div>
+            </div>
+          </PageContainer>
         </div>
       )}
       {currentTab === "종합정보" && (
@@ -53,47 +113,47 @@ export default function StockItemDetail() {
             <div className="grid grid-cols-11">
               <div className="col-span-3 flex justify-between">
                 <div>현재가</div>
-                <div>124</div>
+                <div>{stock.currentPrice}</div>
               </div>
               <div className="col-start-5 col-end-8 flex justify-between">
                 <div>전일대비</div>
-                <div>124</div>
+                <div>{stock.volatility}</div>
               </div>
               <div className="col-start-9 col-end-12 flex justify-between">
                 <div>등락률</div>
-                <div>124</div>
+                <div>{stock.volatilityRate}</div>
               </div>
               <div className="col-span-3 flex justify-between">
                 <div>전일</div>
-                <div>124</div>
+                <div>{stock.yesterdayPrice}</div>
               </div>
               <div className="col-start-5 col-end-8 flex justify-between">
                 <div>고가</div>
-                <div>124</div>
+                <div>{stock.highPrice}</div>
               </div>
               <div className="col-start-9 col-end-12 flex justify-between">
                 <div>거래량</div>
-                <div>124</div>
+                <div>{stock.volume}</div>
               </div>
               <div className="col-span-3 flex justify-between">
                 <div>시가</div>
-                <div>124</div>
+                <div>{stock.marketPrice}</div>
               </div>
               <div className="col-start-5 col-end-8 flex justify-between">
                 <div>저가</div>
-                <div>124</div>
+                <div>{stock.lowPrice}</div>
               </div>
               <div className="col-start-9 col-end-12 flex justify-between">
                 <div>거래대금</div>
-                <div>124</div>
+                <div>{stock.transactionPrice}</div>
               </div>
               <div className="col-span-3 flex justify-between">
                 <div>52주 최고</div>
-                <div>124</div>
+                <div>{stock.highPrice52}</div>
               </div>
               <div className="col-start-5 col-end-8 flex justify-between">
                 <div>52주 최저</div>
-                <div>124</div>
+                <div>{stock.lowPrice52}</div>
               </div>
             </div>
           </PageContainer>
