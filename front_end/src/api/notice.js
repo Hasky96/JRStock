d0e91a8d1effe41f61e5e84c342a5f777d2cb3ab
이재만 <lbefull@naver.com>
2022-03-18 10:58:25 +0900

    #S06P21S001-133 fix: api 요청 전 토큰이 채워지는 문제 수정
    Former-commit-id: 55799fdb82dccf3375438a29e6a7390780a29271 [formerly efde9d86888bc7fba9d7ed1ef13d71d9a078d45a] [formerly 21d10125b7168f61de3f6bb3314d7de9c67913ce [formerly a397d4c09b042731d15768a49595dae49175d73c]]
    Former-commit-id: 21d10125b7168f61de3f6bb3314d7de9c67913ce
    Former-commit-id: 691441298d008340adc8f6ebb92c45e0363c92e0

--- front_end/src/api/notice.js
+++ front_end/src/api/notice.js
@@ -1,14 +1,14 @@
-import { apiInstance } from "./index.js";
-
-const api = apiInstance();
+import { apiInstance, loginApiInstance } from "./index.js";
 
 // 공지사항 전체 조회
 async function getItems(pageNo, pageSize) {
+  const api = apiInstance();
   return (await api.get(`/notice/?page=${pageNo}&size=${pageSize}`)).data;
 }
 
 // 공지사항 id 조회
 async function getItem(id) {
+  const api = apiInstance();
   return (await api.get(`/notice/${id}`)).data;
 }
 
--- front_end/src/api/stock.js
+++ front_end/src/api/stock.js
@@ -1,15 +1,14 @@
 import { apiInstance, loginApiInstance } from "./index.js";
 
-const api = apiInstance();
-const authApi = loginApiInstance();
-
 // 주식 종목 리스트 받아오기
 async function getStockItemList(page, size) {
+  const api = apiInstance();
   return await api.get(`stock/kospi/?page=${page}&size=${size}`);
 }
 
 // 종목 토론 리스트 받아오기
 async function getBoardList(code, page, size) {
+  const api = apiInstance();
   return await api.get(
     `stock/post/${code}?` +
       (page ? `page=${page}&` : "") +
@@ -19,6 +18,7 @@ async function getBoardList(code, page, size) {
 
 // 토론 게시글 작성
 async function createBoard(title, content, code_number) {
+  const authApi = loginApiInstance();
   return await authApi.post("stock/post/create", {
     title,
     content,
@@ -28,16 +28,19 @@ async function createBoard(title, content, code_number) {
 
 // 토론 게시글 상세페이지 받아오기
 async function getBoardDetail(boardId) {
+  const api = apiInstance();
   return await api.get(`stock/post/detail/${boardId}`);
 }
 
 // 토론 게시글 삭제
 async function deleteBoard(boardId) {
+  const authApi = loginApiInstance();
   await authApi.delete(`stock/post/delete/${boardId}`);
 }
 
 // 토론 게시글 수정
 async function updateBoard(title, content, boardId) {
+  const authApi = loginApiInstance();
   await authApi.put(`stock/post/update/${boardId}`, { title, content });
 }
 
