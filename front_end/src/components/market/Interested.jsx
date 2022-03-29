efac44e6ca289a23544a8ddc5aaf7350815500d4
이재만 <lbefull@naver.com>
2022-03-29 13:22:55 +0900

    #S06P21S001-362 feat: 주요 시세 페이지 실시간 데이터 받아오기
    Former-commit-id: 04b4e890eada62324c99e0e7765c2a4d8cdf75e2 [formerly 513ddc6dfa3dccc0952eea793cb9df6c8f2bff78] [formerly 793b20ae6ef6e9135b246de66038fe1152f880a5 [formerly ff9b0cff1a2bbe1300f14dda282b0350cb5d5af6]]
    Former-commit-id: 793b20ae6ef6e9135b246de66038fe1152f880a5
    Former-commit-id: d28ad6f5911ab4a7fa979df21a2c76f373d81a11

--- front_end/src/components/market/Interested.jsx
+++ front_end/src/components/market/Interested.jsx
@@ -1,15 +1,43 @@
-import { getInterest } from "../../api/stock";
-import { useEffect } from "react";
+import { getInterest, getLive } from "../../api/stock";
+import { useEffect, useState } from "react";
 
 export default function Interested() {
+  const [stocks, setStocks] = useState([]);
+  const [lives, setLives] = useState([]);
+
   const init = async () => {
     const res = await getInterest({ page: 1, size: 5 });
-    console.log(res.data);
+    setStocks(res.data.results);
+
+    let temp = [];
+    for (let stock of res.data.results) {
+      let liveRes;
+      try {
+        const res = await getLive(stock.financial_info.basic_info.code_number);
+        liveRes = res.data;
+        liveRes["isError"] = false;
+      } catch {
+        liveRes = {};
+        liveRes["isError"] = true;
+      }
+      liveRes["company_name"] = stock.financial_info.basic_info.company_name;
+      temp.push(liveRes);
+    }
+    setLives(temp);
+  };
+
+  const updateLive = async () => {
+    console.log(stocks);
   };
 
   useEffect(() => {
     init();
-  });
-  
-  return <div>관심종목</div>;
+    // updateLive();
+  }, []);
+
+  return (
+    <div>
+      <div>관심종목</div>
+    </div>
+  );
 }
