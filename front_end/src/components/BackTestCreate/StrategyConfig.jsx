926c5fd248570d5e53b1ccb62e9240db8f5b1830
Ji Wook Jeong <doksakim77@naver.com>
2022-03-31 00:52:25 +0900

    #S06P21S001-380 feat: tooltip 자료 수집 및 화면에 적용
    Former-commit-id: 02233cbfec87a247e7f45fef9b60cd47d6fe174d [formerly 08303234ffd65cd19cb1a3c15de816e331a82b86] [formerly 9c26d057c35029600e60e4720eacf0f99ed38076 [formerly b4d5d5798b19b5130f596fcf79f0acf3c3471e90]]
    Former-commit-id: 9c26d057c35029600e60e4720eacf0f99ed38076
    Former-commit-id: 73ca18071bb1aafe64a3dc23819ed0a1f591c407

--- front_end/src/components/BackTestCreate/StrategyConfig.jsx
+++ front_end/src/components/BackTestCreate/StrategyConfig.jsx
@@ -68,7 +68,10 @@ export default function StrategyConfig({ values, setValues, type, index }) {
 
   const paintLevelOne = options.map((option, index) => (
     <li key={index} className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-      <button className="w-full text-left flex items-center outline-none focus:outline-none">
+      <button
+        type="button"
+        className="w-full text-left flex items-center outline-none focus:outline-none"
+      >
         <span className="pr-1 flex-1">{strategies[option[1]]}</span>
         <span className="mr-auto">
           <DropdownRight />
@@ -87,7 +90,10 @@ min-w-32
 
   return (
     <div className="group relative">
-      <button className="w-full outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32">
+      <button
+        type="button"
+        className="w-full outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
+      >
         <div className="pr-1 flex flex-col flex-1 justify-center">
           <div className="pr-1 font-semibold flex-1">{strategy}</div>
           <div className="pr-1 font-semibold text-sm text-gray-500 flex-1 relative">
---
+++ front_end/src/components/commons/ToolContent2.jsx
@@ -0,0 +1,81 @@
+import {
+  srcDict,
+  altDict,
+  contentDict,
+  linkDict,
+} from "../BackTestCreate/tooltipData";
+import { strategies, details, paramDict } from "../../config/backtestConfig";
+
+export default function ToolContent2({ sId }) {
+  const paragraphList = (str) => {
+    return str.split("/n").map((content, idx) => {
+      return (
+        <p
+          className=" text-base leading-normal text-gray-600 my-2 text-ellipsis overflow-hidden whitespace-pre-wrap"
+          key={idx}
+        >
+          {content}
+        </p>
+      );
+    });
+  };
+
+  const paramsList = (params) => {
+    const list = [];
+    for (const key in params) {
+      if (key === "0") continue;
+      list.push(
+        <li key={key}>
+          <span className="font-bold">{`${paramDict[key]} `}</span>
+          <span>{`${params[key]}`}</span>
+        </li>
+      );
+    }
+    list.push(
+      <li key={0}>
+        <span className="font-bold">{"weight "}</span>
+        <span>{`${params[0]}`}</span>
+      </li>
+    );
+
+    return list;
+  };
+
+  const contents = () => {
+    return (
+      <div className="content-item mb-5">
+        <div className="flex justify-between mb-5 ">
+          <div className="pr-1 flex flex-col flex-1 justify-center text-center">
+            <div className="text-xl flex-1 font-semibold text-gray-800 whitespace-pre-wrap ">
+              {`${strategies[sId.split("0")[0]]}`}
+            </div>
+            <div className="text-sm font-semibold flex-1 text-gray-500 ">{`${
+              details[sId.split("0")[1]]
+            }`}</div>
+          </div>
+        </div>
+        <div className="grid justify-center items-center">
+          <img
+            className="max-w-[400px] max-h-[400px] rounded-lg"
+            src={require(`../../assets/${srcDict[sId]}`)}
+            alt={altDict[sId]}
+          />
+          {paragraphList(contentDict[sId].info)}
+          {<ul>{paramsList(contentDict[sId].params)}</ul>}
+        </div>
+        <button
+          className="float-right bg-primary text-white rounded-lg px-2 py-2 hover:bg-active duration-300"
+          onClick={(e) => {
+            e.preventDefault();
+            // 위키로 이동
+            window.open(linkDict[sId], "_blank");
+          }}
+        >
+          자세히 보기
+        </button>
+      </div>
+    );
+  };
+
+  return <div className="pb-6">{contents()}</div>;
+}
