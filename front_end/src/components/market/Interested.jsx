bc4a142a5f1b80bca552869a9824a3269b3797b1
이재만 <lbefull@naver.com>
2022-03-30 16:07:41 +0900

    #S06P21S001-361 feat: 주요 시세 관심 종목 로딩 전 스피너 추가
    Former-commit-id: 0c9dfc758844e31bf1155fc5b0c682c9186f1979 [formerly 1cfa87b07141dc539e1acb504b732726081fac92] [formerly c6537d3c210a26e87431ea638ac18581cf792011 [formerly d9da74e7a12032ad2fd2b6160d7f834ddac37402]]
    Former-commit-id: c6537d3c210a26e87431ea638ac18581cf792011
    Former-commit-id: 84b3803c3b56f097ccf5ee9c8c51e778732270a4

--- front_end/src/components/market/Interested.jsx
+++ front_end/src/components/market/Interested.jsx
@@ -10,6 +10,7 @@ export default function Interested() {
   const [refTime, setRefTime] = useState("");
   const [stockCount, setStockCount] = useState(-1);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [isInit, setIsInit] = useState(true);
   const navigate = useNavigate();
 
   const init = async () => {
@@ -34,6 +35,7 @@ export default function Interested() {
       temp.push(liveRes);
     }
     setLives(temp);
+    setIsInit(false);
   };
 
   useEffect(() => {
@@ -234,7 +236,7 @@ export default function Interested() {
                 >
                   <div>
                     <Menu.Button>
-                    {/* <DotsVerticalIcon className="w-5 hover:border-2 hover:border-white cursor-pointer" /> */}
+                      {/* <DotsVerticalIcon className="w-5 hover:border-2 hover:border-white cursor-pointer" /> */}
                       <svg
                         version="1.1"
                         viewBox="0 0 24 24"
@@ -355,19 +357,40 @@ export default function Interested() {
             </div>
           )}
         </div>
-        <div>
-          {stockCount === 0 ? (
-            <div className="flex my-10 text-center w-full justify-center text-gray-400">
-              관심 종목이 없습니다.&nbsp;
-              <div className="cursor-pointer underline" onClick={goStockList}>
-                종목 리스트
+        {isInit ? (
+          <div className="flex justify-center">
+            <svg
+              role="status"
+              className="w-14 h-14 text-gray-200 animate-spin dark:text-gray-300 fill-primary mt-10"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          </div>
+        ) : (
+          <div>
+            {stockCount === 0 ? (
+              <div className="flex my-10 text-center w-full justify-center text-gray-400">
+                관심 종목이 없습니다.&nbsp;
+                <div className="cursor-pointer underline" onClick={goStockList}>
+                  종목 리스트
+                </div>
+                에서 추가해 보세요!
               </div>
-              에서 추가해 보세요!
-            </div>
-          ) : (
-            liveList()
-          )}
-        </div>
+            ) : (
+              liveList()
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
