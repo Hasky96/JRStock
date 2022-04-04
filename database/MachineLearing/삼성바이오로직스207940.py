9b33e84e5cb3c5f794f7d385e7f1064cacfc1902
Haseok Jang <haseok1020@naver.com>
2022-04-04 18:08:15 +0900

    feature/predict close_price
    Former-commit-id: 6342d58750153c02137c573647f398bac684864f [formerly 5a41a23ebfab97f13b6d6142690ecd22cc084de5] [formerly 8aab9e2069fde3cf02e6f0b7fab33b8302bb8559 [formerly 16b9b4c0d12632d521150df5e1cf488f783cf806]]
    Former-commit-id: 8aab9e2069fde3cf02e6f0b7fab33b8302bb8559
    Former-commit-id: 106a65d27f2c0986031ae55b0bfb70509a3d8826

---
+++ database/MachineLearing/삼성바이오로직스207940.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from pandas_datareader import data as pdr
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
+
+from datetime import datetime, timedelta
+
+import os
+
+
+# 이건 없어도 되는내용입니다. 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+   
+
+
+def predict_close():
+    df = pd.DataFrame([['Min',474500.0,483000.0, 473000.0, 0.0, 477000.0, 468700.0]
+                       ,['Max', 1017000.0,1047000.0,991000.0, 3037570.0,1012000.0, 995000.0]
+                       ],columns=['index' ,'Open', 'High', 'Low', 'Volume', 'Close', 'ma_5' ])
+    df = df.set_index('index')
+    
+    model = tf.keras.models.load_model('삼성바이오로직스.h5')
+    print(df.loc['Max'])
+    print(df.loc['Min'])
+    
+    today = datetime.now()
+    end = (today - timedelta(days=1)).date()
+    start = (today- timedelta(days=30)).date()
+    
+    test_case = pdr.get_data_yahoo("207940.KS", start=start, end=end)
+    test_case = test_case[['Open', "High", "Low",  "Volume", "Close"]]
+    test_case['ma_5'] = test_case["Close"].rolling(window=5).mean()
+    test_x = test_case[-10:]
+    
+    numerator = test_x - df.loc['Min']
+    denominator = df.loc['Max'] - df.loc['Min']
+    test_x = numerator / (denominator + 1e-7)   
+    
+    result = model.predict([test_x.values.tolist()])
+    
+    denominator = df.loc['Max']['ma_5'] - df.loc['Min']['ma_5']
+    result_ma5 = result.mean() * (denominator + 1e-7) + df.loc['Min']['ma_5']
+
+    result_close = result_ma5*5 - sum(test_case.iloc[-4:].Close)
+    # print(f'{today.date()} predicted close : {result_close}')
+    # print(test_case.iloc[-1].Close)
+    return result_close
+
+
+print(predict_close())
